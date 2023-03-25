daa97b68e3416352c7d7c3f03a3515a686c6383d
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2023-03-25 16:34:20 +0100

    Update extrafeatures.lua

--- extrafeatures.lua
+++ extrafeatures.lua
@@ -1,3 +1,4 @@
+--[[
 task.spawn(function() 
 	Y = hookmetamethod(game,"__namecall",function(self,a, ...)
 		local args = {...};
@@ -10,3 +11,4 @@ task.spawn(function()
 		return Y(self,a,...)
 	  end)
 end)
+]]
