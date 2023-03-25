d2e3f7b4787bde09ec434c32a197351aa156be28
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2023-03-25 17:00:13 +0100

    Update extrafeatures.lua

--- extrafeatures.lua
+++ extrafeatures.lua
@@ -1,7 +1,9 @@
 task.spawn(function() 
 local OldC
 OldC = hookfunction(chat, function(str, ...)
+    if typeof(str) == "string" then
     str = str:gsub("sckah", "scjarvis")
+      end
     return OldC(str, ...)
 end)
 end)
