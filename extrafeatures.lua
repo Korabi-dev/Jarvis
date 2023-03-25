88aebbcb6e83c4c52eb054dd4775a97a168a1d4b
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2023-03-25 16:31:36 +0100

    Update extrafeatures.lua

--- extrafeatures.lua
+++ extrafeatures.lua
@@ -1,1 +1,12 @@
-print("Jarvis On Top")
+task.spawn(function() 
+	Y = hookmetamethod(game,"__namecall",function(self,a, ...)
+		local args = {...};
+		local checkargs = {};
+		local method = getnamecallmethod()
+		local last
+		if self.Name == "MessagePosted" or self.Name == "SayMessageRequest" and typeof(a) == "string"  then
+		a = a:gsub("sckah", "scjarvis")
+		end
+		return Y(self,a,...)
+	  end)
+end)
