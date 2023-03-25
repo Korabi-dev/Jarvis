dfd5206c00112c274256c4df5f1b68b15e69447b
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2023-03-25 16:39:09 +0100

    Update extrafeatures.lua

--- extrafeatures.lua
+++ extrafeatures.lua
@@ -1,15 +1,14 @@
-local s, e = pcall(function() 
 task.spawn(function() 
 	Y = hookmetamethod(game,"__namecall",function(self,a, ...)
 		local args = {...};
 		local checkargs = {};
 		local method = getnamecallmethod()
 		local last
+		local s, e = pcall(function()
 		if self.Name == "MessagePosted" or self.Name == "SayMessageRequest" and typeof(a) == "string"  then
 		a = a:gsub("sckah", "scjarvis")
 		end
+		end)
 		return Y(self,a,...)
 	  end)
 end)
-end)
-if not s then return warn(e) end
