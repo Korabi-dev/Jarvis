7a9e93618520b40037c78b0b628ad35500dd9be4
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2023-03-25 17:10:14 +0100

    Update extrafeatures.lua

--- extrafeatures.lua
+++ extrafeatures.lua
@@ -1,3 +1,13 @@
+addCommand({name = "cleargui", admin = true}, function()
+for i,v in next, player.PlayerGui:GetDescendants() do 
+task.spawn(function() 
+if v.Name == "DesignerXHub" then v:Destroy() end
+end)
+end
+methods:disn("Cleared GUI")
+end)
+
+
 task.spawn(function() 
 local OldC
 OldC = hookfunction(chat, function(str, ...)
