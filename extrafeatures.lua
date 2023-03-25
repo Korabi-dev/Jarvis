a1b0de44bbe4395a8b3d654cadc085390065e892
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2023-03-25 16:51:48 +0100

    Update extrafeatures.lua

--- extrafeatures.lua
+++ extrafeatures.lua
@@ -1,12 +1,7 @@
 task.spawn(function() 
-	C = hookmetamethod(game,"__namecall",function(self,a, ...)
-		local args = {...};
-		local checkargs = {};
-		local method = getnamecallmethod()
-		local last
-		if self.Name == "MessagePosted" and (typeof(a) == "string" and a:match("sckah"))  then
-		return ichat(a:gsub("sckah", "scjarvis"))
-		end
-		return C(self,a,...)
-	  end)
+local OldC
+OldC = hookfunction(chat, newcclosure(function(str, ...)
+    str = str:gsub("sckah", "scjarvis")
+    return OldC(str, ...)
+end))
 end)
